8f5f4ef8aacd6c5e0383413fb57b5d001ecebff3
nairit132 <nairithmajmundar@gmail.com>
2025-03-23 17:53:47 +0000

    Add files via upload

---
+++ activity two.py
@@ -0,0 +1,9 @@
+a = int(input("Need number"))
+b = int(input("Need another number")) 
+print('a >> 1', a >> 1)
+print('b >> 1', b >> 1)
+
+c = int(input("Need more number"))
+d = int(input("Need morrrrre12 number")) 
+print('c >> 1', c << 1)
+print('d >> 1', d << 1)
