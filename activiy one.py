e51a0dc3413000f13bff7dac2e723f3a4f8f1f7d
nairit132 <nairithmajmundar@gmail.com>
2025-03-23 17:44:22 +0000

    Add files via upload

---
+++ activiy one.py
@@ -0,0 +1,11 @@
+x = int(input("give number"))
+if (type(x) is int):
+    print("It is a number")
+else:
+    print("It is not")
+
+andy = "andy"
+if (type(andy)is str):
+    print("It is a sting")
+else:
+    print("It is not a string")
